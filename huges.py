a5ebd3fe8b36d68118e8bdc1b9fa2dac569b8367
lazrobadzo <144172921+lazrobadzo@users.noreply.github.com>
2023-10-29 08:49:23 -0700

    bnhvh
    jbj

---
+++ huges.py
@@ -0,0 +1,25 @@
+x =[2, 4, 6, 2, 5, 2, 7 ]
+
+for i in range (2) :
+
+    x.remove (5)
+    
+    print(x)
+
+
+
+
+    meore=("davalebac aq gavakete")
+
+    m_age=[45 , 42 ,34 ]
+
+    full_sent="mamacemis asaki {} ,  dedas asaki {},   cemi asaki {}". format(m_age[0] , m_age [1] m_age[2])
+
+    print(full_sent)
+
+f_age=[45+10 , 42+10 , 34+10]
+
+ful_sent_1="mamacemis asaki {}, dedas asaki {}, cemi asaki {}", format( m_age[0], m_age[1], m_age[2])   
+
+
+print(full_sent 1)
